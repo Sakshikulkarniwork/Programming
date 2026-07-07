9edef66792c945d7927c859227ef4077234f99a9
Sakshikulkarniwork <kulkarnisakshiwork@gmail.com>
2026-07-07 21:49:49 +0530

    Add files via upload

---
+++ Python/Program1.py
@@ -0,0 +1,22 @@
+#2 Q1 WAP to display value, type and memory address
+
+no = 11
+marks = 90.78
+gender = True
+demo = None
+
+print(no)        #value
+print(type(no))  #type
+print(id(no))    #memory address
+
+print(marks)       
+print(type(marks)) 
+print(id(marks))   
+
+print(gender)       
+print(type(gender)) 
+print(id(gender)) 
+
+print(demo)       
+print(type(demo)) 
+print(id(demo)) 
---
+++ Python/Program3.py
@@ -0,0 +1,6 @@
+#2 Q9 WAP to take user's name and age and display
+
+Name = input("Enter your name : ")
+Age = int(input("Enter your age : "))
+
+print("Hello", Name, ", you will turn", Age+1, "next year.")
