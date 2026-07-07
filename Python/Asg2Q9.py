086ab31401b420725f54081589eaedb81a313e12
Sakshikulkarniwork <kulkarnisakshiwork@gmail.com>
2026-07-07 20:54:30 +0530

    Delete Python/Asg2Q9.py

--- Python/Asg2Q9.py
+++
@@ -1,6 +0,0 @@
-# Q.9) WAP to take user's name and age and display
-
-Name = input("Enter your name : ")
-Age = int(input("Enter your age : "))
-
-print("Hello", Name, ", you will turn", Age+1, "next year.")
