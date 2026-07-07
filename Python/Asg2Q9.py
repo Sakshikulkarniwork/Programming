4d85b5a22df8df83185b6e0c31eeaeec7ff3edc7
Sakshikulkarniwork <kulkarnisakshiwork@gmail.com>
2026-07-07 20:00:06 +0530

    Add files via upload

---
+++ Python/Asg2Q9.py
@@ -0,0 +1,6 @@
+# Q.9) WAP to take user's name and age and display
+
+Name = input("Enter your name : ")
+Age = int(input("Enter your age : "))
+
+print("Hello", Name, ", you will turn", Age+1, "next year.")
